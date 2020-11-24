0fa716815f43562307a55d41d8699e5b712f26b9
rally5236 <74947637+rally5236@users.noreply.github.com>
2020-11-24 13:33:08 +0700

    Update xy.py

--- xy.py
+++ xy.py
@@ -2,11 +2,24 @@ def eq(x,y):
     count=0
     times ="{} times"
     while x != y:
-        if x >y/2:
-           x-=1
-           count+=1
+        if y%2==0:    
+            if x >y/2:
+               x-=1
+               count+=1
+            else:
+                x*=2
+                count+=1
         else:
-            x*=2
-            count+=1
+            if x > (y+1)/2:
+                x-=1
+                count+=1
+            elif x <(y+1)/2:
+                x*=2
+                count+=1
+            elif x == (y+1)/2:
+                x*=2
+                count+=1
+                x-=1
+                count+=1
     print(times.format(count))
-eq(int(input()),int(input()))1
+eq(int(input()),int(input()))
