9a38f0fceed1cdb7e46ff46ee14eadbef405a0e4
rally5236 <74947637+rally5236@users.noreply.github.com>
2020-11-25 08:43:26 +0700

    Create table in mysql readfile csv

--- mysql.py
+++ mysql.py
@@ -6,7 +6,7 @@ connection = MySQLdb.connect(host='localhost',
     passwd='',
     db='customerdb')
 cursor = connection.cursor()
-
+ 
 CustomerTableSql="""CREATE TABLE Customers(customerid INT(20) PRIMARY KEY,
 firstname CHAR(20) NOT NULL,lastname CHAR(20),companyname CHAR(20),
 billingaddress1 CHAR(50),billingaddress2 CHAR(50),city CHAR(20),state CHAR(10),
