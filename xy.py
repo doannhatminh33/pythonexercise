1a838abbc8f9128cc6f06010c6dc68bda3bf52e2
rally5236 <74947637+rally5236@users.noreply.github.com>
2020-11-25 08:41:40 +0700

    Convert x to y shortest count

--- xy.py
+++ xy.py
@@ -1,12 +1,12 @@
 def eq(x,y):
-    if(x==y):
+    if(x == y):
         return 0
-    if(x>y):
+    if(x > y):
         return x-y
-    if y%2==0:    
+    if y % 2 == 0:    
         return 1 + eq(x, y/2)       
     else:
        return 1 + eq(x, y+1)
-x=int(input())
-y=int(input())
+x = int(input())
+y = int(input())
 print(eq(x,y))
