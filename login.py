b0d377f7111521886a600b6ac28438468fc93384
rally5236 <74947637+rally5236@users.noreply.github.com>
2020-11-25 08:45:39 +0700

    Get current user by requests

--- login.py
+++ login.py
@@ -3,7 +3,7 @@ import requests
 url = 'http://45.79.43.178/source_carts/wordpress/wp-login.php'
 values = {'log': 'admin',
           'pwd': '123456aA'}
-
+ 
 session = requests.Session()
 resp    = session.post(url,data=values)
 tree = html.fromstring(resp.content)
