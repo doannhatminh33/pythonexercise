370a9130fb1f6f8ee66b6536f21d064577ffefe2
rally5236 <74947637+rally5236@users.noreply.github.com>
2020-11-25 08:44:55 +0700

    Get current user by selenium

--- seleniumm.py
+++ seleniumm.py
@@ -4,7 +4,7 @@ driver = webdriver.Chrome()
 url = 'http://45.79.43.178/source_carts/wordpress/wp-login.php'
 values = {'log': 'admin',
           'pwd': '123456aA'}
-
+ 
 driver.get(url)
 driver.find_element_by_name("log").send_keys('admin')
 driver.find_element_by_name("pwd").send_keys('123456aA')
